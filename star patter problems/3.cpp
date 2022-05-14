a62028d93d6538b40f914c6caea131f60fb3fb3a
MAV <mohsinali530121@gmail.com>
2022-05-14 23:24:32 +0530

    stars problems

---
+++ star patter problems/3.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=2*n;j++)
+        {
+            if(i>=2 && j>=n+2-i && j<=n+i-1) cout<<"*" ; 
+            else 
+            {
+                if(j<=5) cout<<j;
+                else cout<<2*n+1-j;
+            }
+        }
+        cout<<endl;
+    }
+   return 0;
+}
