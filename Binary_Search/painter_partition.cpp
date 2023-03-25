f4353b10b2cfbea979cee1703296d1bce44acb74
MAV <mohsinali530121@gmail.com>
2023-03-25 17:04:16 +0530

    Binary Search

--- Binary_Search/painter_partition.cpp
+++ Binary_Search/painter_partition.cpp
@@ -1,4 +1,5 @@
 // https://www.codingninjas.com/codestudio/problems/painter's-partition-problem_1089557?source=youtube&campaign=love_babbar_codestudio2&utm_source=youtube&utm_medium=affiliate&utm_campaign=love_babbar_codestudio2&leftPanelTab=0
+
 #include <bits/stdc++.h>
 using namespace std;
 bool isPossible(vector<int> &boards, int k,int mid)
